9ef689c75420f931bc97619e73edb63c35f90510
Igor Leontyev <crybzleon@yandex.ru>
2023-02-10 23:00:14 +0300

    test

---
+++ task0/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("input first number");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input second number");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+
+if(numberSecond * numberSecond == numberOne){
+    Console.WriteLine("YES");
+}
+else
+    {
+       Console.WriteLine("NO"); 
+    }
+
+
+
+
+
+
+Console.WriteLine("input number DAY ");
+int numDay = Convert.ToInt32(Console.ReadLine());
+
+switch(numDay)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+    case 7:
+        Console.WriteLine("Sunday");
+        break;
+    default:
+        Console.WriteLine("unknown number");
+        break;
+}
+
---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("input number");
+int num = Convert.ToInt32(Console.ReadLine());
+int buf = num - 1 ;
+buf = buf - (num * 2);
+
+while (buf < num)
+{
+    buf++;
+    Console.Write(buf+"\t");
+   
+}
+Console.WriteLine("\n");
+
+
+Console.WriteLine("input num");
+int numb = Convert.ToInt32(Console.ReadLine());
+int su = numb % 10;
+Console.WriteLine(su);
